115af7cb2ea1c71c3b0177eb1bb82c592d5f73d9
deel18 <deel18.github@gmail.com>
2019-12-16 18:21:23 +0000

    Created new class for handling cURL fetches to darkskyapi

---
+++ src/Curl/Curl.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Curl, a class supporting the darkssky API.
+ */
+
+namespace Anax\Curl;
+
+use Anax\Commons\ContainerInjectableInterface;
+use Anax\Commons\ContainerInjectableTrait;
+
+class Curl
+{
+
+    use ContainerInjectableTrait;
+
+    public function weather($latitude, $longitude)
+    {
+        $apiKey = file_get_contents(ANAX_INSTALL_PATH . "/config/apikey.txt", FALSE, NULL, 33, 33);
+        $apiKey = trim($apiKey);
+        $curl = curl_init();
+
+        curl_setopt($curl, CURLOPT_URL, "https://api.darksky.net/forecast/$apiKey/$latitude,$longitude?exclude=minutely,hourly,alerts,flags");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $result = curl_exec($curl);
+
+        $decode = json_decode($result);
+
+        return $decode;
+    }
+}
