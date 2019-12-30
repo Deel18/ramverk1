2b76e1af2d4310ab3db02f31cbb5fe4547208073
deel18 <deel18.github@gmail.com>
2019-12-30 20:11:50 +0000

    added support for weather json format

---
+++ view/weatherjson/verify.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Anax\View;
+
+/**
+ * Render content within an article.
+ */
+
+// Show incoming variables and view helper functions
+//echo showEnvironment(get_defined_vars(), get_defined_functions());
+
+
+?>
+<h1>Weather JSON</h1>
+
+<p>Enter an IP/Geolocation to check the weather.</p>
+
+<form action="weatjson/weatherjson">
+    <input type="text" name="ip">
+    <br>
+    <br>
+    Latitude
+    <br>
+    <input type="text" name="latitude">
+    </label>
+    <br>
+    <br>
+    <label>
+    Longitude
+    <br>
+    <input type="text" name="longitude">
+    </label>
+    <br>
+    <br>
+    <label>
+    <input type="submit" name="verify" value="Verify">
+</form>
+<br>
+<br>
